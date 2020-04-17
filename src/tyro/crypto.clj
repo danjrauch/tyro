e860d43a1450de8350e3246e78eb0c131ebf4c4a
Dan Rauch <drauch@hawk.iit.edu>
2020-04-16 21:45:02 -0500

    init crytpo

--- src/tyro/crypto.clj
+++ src/tyro/crypto.clj
@@ -1,5 +1,7 @@
 (ns tyro.crypto
-  (:require [clojure.math.numeric-tower :as math]))
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.set :as set]
+            [clojure.edn :as edn]))
 
 (defn pow-mod
   " b^e mod m (using Java which solves some cases the pure clojure method has to be modified to tackle--i.e. with large b & e and 
@@ -35,7 +37,7 @@
                       (reset! res false))))))
             @res)))
 
-(is-prime 48383)
+;; (is-prime 48383)
 
 (defn generate-prime-candidate
   ""
@@ -48,7 +50,7 @@
       (inc' n)
       n)))
 
-(generate-prime-candidate)
+;; (generate-prime-candidate)
 
 (defn generate-prime
   ""
@@ -56,9 +58,88 @@
   [& {:keys [minimum maximum]
       :or   {minimum 3000
              maximum (*' minimum 10)}}]
-  (let [p (atom (bigint (generate-prime-candidate :minimum minimum)))]
+  (let [p (atom (bigint (generate-prime-candidate :minimum minimum :maximum maximum)))]
     (while (not (is-prime @p))
-      (reset! p (bigint (generate-prime-candidate :minimum minimum))))
+      (reset! p (bigint (generate-prime-candidate :minimum minimum :maximum maximum))))
     @p))
 
-(generate-prime :minimum 2000)
+;; (generate-prime :minimum 2000)
+
+(defn bytes->bigint
+  [bytes & {:keys [little-endian]
+            :or {little-endian true}}]
+  (let [b (if little-endian (reverse bytes) bytes)]
+    (->> b
+         (cons (byte 0))
+         (byte-array)
+         (bigint))))
+
+;; (bytes->bigint (.getBytes (prn-str {:type 4})))
+
+(defn bigint->bytes
+  [n & {:keys [little-endian]
+        :or {little-endian true}}]
+  (let [bytes (->> n
+                   biginteger
+                   .toByteArray)]
+    (if little-endian (byte-array (reverse bytes)) (byte-array bytes))))
+
+;; (.getBytes (prn-str {:type 4}))
+;; (bigint->bytes (bytes->bigint (.getBytes (prn-str {:type 4}))))
+
+(defn common-factors 
+  [n m]
+  (into (sorted-set)
+        (for [x (range 1 (inc (math/gcd n m)))
+              :when (zero? (rem (math/gcd n m) x))]
+          x)))
+
+;; (common-factors 4 8)
+
+(defn generate-rsa-keypair
+  "Generate a new public key for RSA."
+  {:added "0.4.0"}
+  []
+  (let [e 3
+        n (atom -1)
+        d (atom -1)
+        block-size (atom 1)]
+    (while (== @d -1)
+      (let [p (generate-prime :minimum 100000000000000N)
+            q (generate-prime :minimum 100000000000000N)
+            phi (*' (dec p) (dec q))]
+        (when (= (common-factors e phi) #{1})
+          (reset! n (*' p q))
+          (reset! d (bigint (.modInverse (biginteger e) (biginteger phi))))
+          (while (<= (bytes->bigint (byte-array (repeat @block-size 127))) (dec @n))
+            (swap! block-size inc))
+          (swap! block-size dec))))
+    (zipmap [:e :n :d :block-size] [e @n @d @block-size])))
+
+;; (generate-rsa-keypair)
+
+(defn encrypt
+  "Encrypt a message."
+  {:added "0.4.0"}
+  [m kp block-size]
+  (let [byte-list (cond
+                    (= (type m) (Class/forName "[B")) m
+                    :else (.getBytes (prn-str m)))]
+    (reduce merge (for [[i block] (map-indexed vector (partition block-size block-size nil byte-list))]
+                    {i (pow-mod (bytes->bigint (byte-array block)) (:e kp) (:n kp))}))))
+
+(defn decrypt
+  "Decrypt a message."
+  {:added "0.4.0"}
+  [cypher d n]
+  (edn/read-string
+   (String. (byte-array (mapcat seq (for [[_ v] (into (sorted-map) cypher)]
+                                      (bigint->bytes (pow-mod v d n))))))))
+
+(let [whole-kp (generate-rsa-keypair)
+      public-kp (dissoc whole-kp :d)
+      private-key (:d whole-kp)
+      plain {:time 1586479272529, :type 3, :port 8100, :host "127.0.0.1", :ttl 1, :success true, :id "I40", :file-name ".DS_Store"}
+      cypher (encrypt plain public-kp (:block-size whole-kp))
+      decypher (decrypt cypher private-key (:n whole-kp))]
+  decypher)
